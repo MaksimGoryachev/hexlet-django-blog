bc5d19cb5f699c52e6ba744b1603a10bdb1557be
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-10-24 16:48:25 +0500

    Update urls.py

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -17,13 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hexlet_django_blog import views
-from .views import BaseView
+# from .views import BaseView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     # path('', views.base),
-    path('', BaseView.as_view(),name='base'),
-    path('about/', views.about),
+    path('', views.BaseView.as_view(), name='base'),
+    path('about/', views.about, name='about'),
     path('articles/', include('hexlet_django_blog.article.urls')),
 ]
