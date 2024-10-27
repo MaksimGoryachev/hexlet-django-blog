0131634e5131a9b1230b24bc9117a667f65b4f0f
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-10-27 18:13:00 +0500

    Create article/models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title=models.CharField(max_length=255)
+    author=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
