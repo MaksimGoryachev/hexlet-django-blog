c463c214ccca4e72e97d50b0eea0b6935c41b5b9
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-10-28 17:37:55 +0500

    Change settings.py

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fl8lh+vy)gq!y^6439ys&!pth17$k)1oaz=oeg*fyu@gxt5w%$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
