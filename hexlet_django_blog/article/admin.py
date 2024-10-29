abc31453dfec63ac9c36ae76bee1d5a2d7adbc5d
MaksimGoryachev <m.goryachev2011@yandex.ru>
2024-10-29 11:37:24 +0500

    Change admin.py, models.py

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'body']
+    list_display = ('name', 'created_at', 'id')
+    list_filter = (('created_at', DateFieldListFilter),)
